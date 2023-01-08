27bddad12354700a6f2bc20c3ce3df7395f49c9a
AnkitVats99 <vatsankit99@gmail.com>
2023-01-09 01:46:46 +0530

    removed serializer from main app

--- youtube_solution/serializer.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from django.db.models.fields import TextField
-from django.core.validators import RegexValidator
-
-class TitleAndDescriptionSerializer(serializers.Serializer):
-    page_number = serializers.IntegerField(default=1,required=False)
-    title = serializers.CharField(required=False)
-    description = serializers.CharField(required=False)
