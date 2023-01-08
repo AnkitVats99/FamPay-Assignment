d764ce924f13e308072d790feeaf31c8c3b00b2c
AnkitVats99 <vatsankit99@gmail.com>
2023-01-09 01:09:12 +0530

    modified cron

--- youtube_solution/cron.py
+++ youtube_solution/cron.py
@@ -11,34 +11,36 @@ class MyCronJob(CronJobBase):
 
     schedule = Schedule(run_every_mins=RUN_EVERY_MINS)
     code = 'youtube_solution.cron.fill_data_in_video_model'    # a unique code
-
+   
     def do(self):
         time_now = datetime.now()
-        last_request_time = time_now - timedelta(minutes=5)
+        last_request_time = time_now - timedelta(minutes=1)
         url='https://youtube.googleapis.com/youtube/v3/search'
         payload={'part': 'snippet',
         'order': 'date',
-        'q': 'news',
+        'q': 'india',
         'maxResults': 1,
         'key':'AIzaSyDMO4xx23T-8SAFPXJrWZGtTIZNXzBg3HQ',
         'publishedAfter':(last_request_time.replace(microsecond=0).isoformat()+'Z')
            }
         res=requests.get(url, params=payload, verify=False).json()
-        for item in res['items']:
-            video_id = item['id']['videoId']
-            publishedDateTime = item['snippet']['publishedAt']
-            title = item['snippet']['title']
-            description = item['snippet']['description']
-            thumbnailsUrls = item['snippet']['thumbnails']['default']['url']
-            channel_id = item['snippet']['channelId']
-            channel_title = item['snippet']['channelTitle']
-            Video.objects.create(
-                video_id=video_id,
-                title=title,
-                description=description,
-                channel_id=channel_id,
-                channel_title=channel_title,
-                publishedDateTime=publishedDateTime,
-                thumbnailsUrls=thumbnailsUrls,
-                created=datetime.now()
-            )
+        if res.get('items'):
+            for item in res['items']:
+                video_id = item['id']['videoId']
+                thumbnailsUrls = item['snippet']['thumbnails']['default']['url']
+                channel_id = item['snippet']['channelId']
+                publishedDateTime = item['snippet']['publishedAt']
+                channel_title = item['snippet']['channelTitle']
+                title = item['snippet']['title']
+                description = item['snippet']['description']
+                
+                Video.objects.create(
+                    video_id=video_id,
+                    title=title,
+                    thumbnailsUrls=thumbnailsUrls,
+                    created=datetime.now(),
+                    description=description,
+                    channel_id=channel_id,
+                    channel_title=channel_title,
+                    publishedDateTime=publishedDateTime
+                )
